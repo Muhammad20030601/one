87a63ebdbe75036d735974d5e710d94e86fd28b4
Muhammad <nosirovn07@gmail.com>
2023-07-11 02:09:51 +0300

    first

---
+++ main.js
@@ -0,0 +1,18 @@
+//alert("Сегодня 10.11.2023")
+document.getElementById("text").innerHTML="Js работает";
+console.log("Добро рожаловать на курс Muhammad");
+console.log(1234);
+console.log("Строка");
+console.log(76376723892379878987978678679n)
+let a=true;
+a=false;
+console.log(a)
+let b=null;
+console.log(b);
+let c=12345;
+c=undefined;
+console.log(c);
+let d=9999;
+console.log(typeof d);
+let e="Список"
+console.log(typeof e);
