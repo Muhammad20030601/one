32fdf9bb35211297649a0b5d630ba57e3a697220
Muhammad <nosirovn07@gmail.com>
2023-07-15 10:10:01 +0300

    Switch case

--- main.js
+++ main.js
@@ -1,20 +1,33 @@
-/*let a,b,c
-while(a<=50){
- console.log(a)
- a++
-}
-a=50
+// let a,b
+// a=1
+// b=prompt("Введите число")
+// switch(b++){
+// case 2:
+// alert("Верно")
+// break
+// case a:
+// alert("Не верно")
+// break
+// default:
+// alert("Не сработало")
+// }
 
-while(a>=25){
-console.log(a)
-  a--
-}*/
+// let a =prompt("Введите число: ");
 
-for(let a=1;a<=50;a++){
-  console.log(a)
-}
+// if (a >= 1 && a <= 3) {
+//   alert("зима");
+// } else if (a >= 4 && a <= 6) {
+//   alert("весна");
+// } else if (a >= 7 && a <= 9) {
+//   alert("лето");
+// } else if (a >= 10 && a <= 12) {
+//   alert("осень");
+// } else {
+//   alert("Ошибка");
+// }
 
-/*for(let b=2500;b<=3001;b+=3){
-  
-  console.log(b);
-}*/
+let a,b,c
+a=1
+b=null
+c=2
+console.log(a??b,c);
