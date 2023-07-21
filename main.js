92122b77a76540dd03403e22d90708fa511193fe
Muhammad <nosirovn07@gmail.com>
2023-07-21 12:12:59 +0300

    calculator

--- main.js
+++ main.js
@@ -1,33 +1,46 @@
-// let a,b
-// a=1
-// b=prompt("Введите число")
-// switch(b++){
-// case 2:
-// alert("Верно")
-// break
-// case a:
-// alert("Не верно")
-// break
-// default:
-// alert("Не сработало")
-// }
+// Напишите функцию именную, которая при нажатии кнопки выводит в блок див на странице значения из переменной A.
 
-// let a =prompt("Введите число: ");
+// Напишите простой калькулятор из 4 выражений + - * / для этого сделайте 4 кнопки и поле ввода, затем при
+//  вводе каждого числа и после при 
+// нажатии на кнопку должна выполнится одна из функций, в зависимости от того какую кнопку мы нажали
 
-// if (a >= 1 && a <= 3) {
-//   alert("зима");
-// } else if (a >= 4 && a <= 6) {
-//   alert("весна");
-// } else if (a >= 7 && a <= 9) {
-//   alert("лето");
-// } else if (a >= 10 && a <= 12) {
-//   alert("осень");
-// } else {
-//   alert("Ошибка");
-// }
 
-let a,b,c
-a=1
-b=null
-c=2
-console.log(a??b,c);
+let e=document.querySelector("#in")
+let button=document.querySelector("#s")
+let di=document.querySelector("#tex")
+button.onclick=function(){
+  di.innerHTML=e.value
+}
+
+
+let n1=document.querySelector("#n1")
+let n2=document.querySelector("#n2")
+let div=document.querySelector("#text")
+let a=document.querySelector("#p")
+let b=document.querySelector("#m")
+let c=document.querySelector("#u")
+let d=document.querySelector("#d")
+a.onclick=function(){
+ div.value=Number(n1.value)+Number(n2.value)
+ div.innerHTML=div.value
+    
+}
+
+b.onclick=function(){
+    div.value=Number(n1.value)-Number(n2.value)
+    div.innerHTML=div.value
+       
+   }
+
+   c.onclick=function(){
+    div.value=Number(n1.value)*Number(n2.value)
+    div.innerHTML=div.value
+       
+   }
+
+   d.onclick=function(){
+    div.value=Number(n1.value)/Number(n2.value)
+    div.innerHTML=div.value
+       
+   }
+
