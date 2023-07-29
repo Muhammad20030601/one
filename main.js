b6758fc9f0933ebab7ee08aabe5c99f89658e057
Muhammad <nosirovn07@gmail.com>
2023-07-29 23:10:56 +0300

    api

--- main.js
+++ main.js
@@ -1,23 +1,19 @@
-// Создать кнопку button 1, которая будет запускать функцию, при которой элементу div с классом element будет задаваться 
-// ширина и высота (размеры произвольные)
-// Добавить 3 дополнительных класса и написать функцию при нажатии на кнопку эти 3 класса будут присваиваться к 1 
-// элементу (элемент произвольный)
-// Написать функцию, которая при клике по кнопке будет удалять любой из 3 классов, что были присвоены во 2 задаче
+let pic=document.querySelector("#tx")
+let email=document.querySelector("#email")
+let cell=document.querySelector("#cell")
+let id=document.querySelector("#id")
+let gender=document.querySelector("#gender")
+fetch('https://randomuser.me/api')
+.then(function(resp){
+  return resp.json();
 
-let bt=document.querySelector("#cl")
-let res=document.querySelector("div")
-bt.onclick=function(){
-  res.classList.toggle("emmet")
+}).then(function(data){
+  console.log(data);
+pic.innerHTML+=data.results[0].name.first
+  pic.innerHTML+='<img src=${data.results[0].picture.large} />'
+  email.innerHTML+=data.results[0].email
+  cell.innerHTML+=data.results[0].cell
+  gender.innerHTML+=data.results[0].gender
 
-}
-
-let bt1=document.querySelector("#cl2")
-let p=document.querySelector("p")
-bt1.onclick=function(){
-  p.classList="color size back"
-}
-
-let bdel=document.querySelector("#del")
-bdel.onclick=function(){
-  p.classList.remove("color")
-}
+  
+})
