e50642fa3bda52c755d806b940c0556e54ab0ef2
Muhammad <nosirovn07@gmail.com>
2023-08-01 22:35:38 +0300

    Promise

--- main.js
+++ main.js
@@ -1,19 +1,32 @@
-let pic=document.querySelector("#tx")
-let email=document.querySelector("#email")
-let cell=document.querySelector("#cell")
-let id=document.querySelector("#id")
-let gender=document.querySelector("#gender")
-fetch('https://randomuser.me/api')
-.then(function(resp){
-  return resp.json();
+let a = new Set();
+let b=1
+a.add("'e' 'r' 'i' 'k' 't' 'h' 'e' 'b' 'e' 's' 't'" )
 
-}).then(function(data){
-  console.log(data);
-pic.innerHTML+=data.results[0].name.first
-  pic.innerHTML+='<img src=${data.results[0].picture.large} />'
-  email.innerHTML+=data.results[0].email
-  cell.innerHTML+=data.results[0].cell
-  gender.innerHTML+=data.results[0].gender
+console.log(a)
 
-  
-})
+
+let bt=document.querySelector("#bt")
+let inp=document.querySelector("#in")
+bt.onclick=function(){
+  console.log(inp.value)
+}
+
+
+let bt2=document.querySelector("#bt2")
+let ot=document.querySelector("#otvet")
+bt2.onclick=function(){ 
+let i=new Set(["Ne pravilno","Pravilno"])
+if(inp.value==7){ 
+ot.innerHTML=i.has("Pravilno")
+}else{ 
+  ot.innerHTML=('false')  
+}
+}
+let pr=document.querySelector("#z3")
+pr.onclick=function(){ 
+let d=[1,2,3,4,5,6]
+for(let key of d){
+  document.querySelector("#p3").innerHTML=d
+}
+
+}
