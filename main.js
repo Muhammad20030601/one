fd62cd50f9901c9791f35e9f5df894e1353e0740
Muhammad <nosirovn07@gmail.com>
2023-08-03 23:49:41 +0300

    End 14

--- main.js
+++ main.js
@@ -1,15 +1,24 @@
-let i=localStorage.setItem("data",15)
-console.log(localStorage.getItem("data"));
+// let i=localStorage.setItem("data",15)
+// console.log(localStorage.getItem("data"));
 
-let htt=new XMLHttpRequest();
-htt.onreadystatechange=function(){
-  if(this.readyState==1 && this == 200)
-  st(this.resText)
-}
+// let htt=new XMLHttpRequest();
+// htt.onreadystatechange=function(){
+//   if(this.readyState==1 && this == 200)
+//   st(this.resText)
+// }
 
-htt.open("GET","https://ok.ru/")
-htt.send("")
+// htt.open("GET","https://ok.ru/")
+// htt.send("")
 
-function st(data){
- console.log("data");
-}
+// function st(data){
+//  console.log("data");
+// }
+
+let i=new Promise((res,rej)=>{
+  fetch("https://ok.ru/").then((data)=>{
+    res(data.text())
+  })
+})
+i.then((data)=>{
+  console.log(data);
+})
