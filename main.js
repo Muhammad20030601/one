00d655619d46b42283f93ee582fc03f03cffe26f
Muhammad <nosirovn07@gmail.com>
2023-08-03 23:05:21 +0300

    End 13

--- main.js
+++ main.js
@@ -1,32 +1,15 @@
-let a = new Set();
-let b=1
-a.add("'e' 'r' 'i' 'k' 't' 'h' 'e' 'b' 'e' 's' 't'" )
+let i=localStorage.setItem("data",15)
+console.log(localStorage.getItem("data"));
 
-console.log(a)
-
-
-let bt=document.querySelector("#bt")
-let inp=document.querySelector("#in")
-bt.onclick=function(){
-  console.log(inp.value)
+let htt=new XMLHttpRequest();
+htt.onreadystatechange=function(){
+  if(this.readyState==1 && this == 200)
+  st(this.resText)
 }
 
+htt.open("GET","https://ok.ru/")
+htt.send("")
 
-let bt2=document.querySelector("#bt2")
-let ot=document.querySelector("#otvet")
-bt2.onclick=function(){ 
-let i=new Set(["Ne pravilno","Pravilno"])
-if(inp.value==7){ 
-ot.innerHTML=i.has("Pravilno")
-}else{ 
-  ot.innerHTML=('false')  
-}
-}
-let pr=document.querySelector("#z3")
-pr.onclick=function(){ 
-let d=[1,2,3,4,5,6]
-for(let key of d){
-  document.querySelector("#p3").innerHTML=d
-}
-
+function st(data){
+ console.log("data");
 }
